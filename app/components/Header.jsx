4d1909d0e892ef47ef138bedd1588b4548a62dea
Ugurtasci <ugurtasci1291@gmail.com>
2024-09-25 01:50:51 +0300

    header ve slider düzenlendi

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,22 +5,14 @@ import { FaPhoneVolume } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <header className="flex justify-between flex-col md:flex-row gap-4   items-center px-20 mt-5">
+    <header className="flex justify-between flex-col lg:flex-row gap-4   items-center px-20 mt-5">
       {/* left logo */}
-      <article className="flex items-center gap-4">
-        <img
-          className="w-24 h-12 md:w-30 md:h-14 xl:w-32 xl:h-20"
-          src="logo.png"
-          alt=""
-        />
-        <img
-          className="w-24 h-14 md:w-30 md:h-16 xl:w-40 xl:h-24 "
-          src="logo2.png"
-          alt=""
-        />
+      <article className=" flex items-center justify-center  lg:gap-2 gap-8">
+        <img className="object-cover w-32 " src="logo.png" alt="" />
+        <img className="object-cover w-44" src="logo2.png" alt="" />
       </article>
       {/* right logo */}
-      <article className="flex items-center gap-4 ">
+      <article className="flex items-center justify-center gap-4">
         <div
           className="flex items-center gap-4 bg-gray-500 text-white py-2 md:px-4 px-2 
          font-semibold xl:text-lg text-sm  rounded-full cursor-pointer hover:bg-gray-700 transition-all"
@@ -30,11 +22,12 @@ const Header = () => {
         </div>
         <div
           className="flex items-center gap-4 bg-red-500 py-2 md:px-4  px-2 rounded-full
-         text-white font-semibold xl:text-lg text-sm cursor-pointer hover:bg-red-700 transition-all"
+         text-white font-semibold xl:text-lg md:text-md text-sm cursor-pointer hover:bg-red-700 transition-all"
         >
           <FaPhoneVolume className=" text-white " />
           <p> +90 534 616 7622</p>
         </div>
+
         <div className="hidden lg:block">
           <Image
             src="/icons/icon-24.svg"
--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -7,14 +7,20 @@ const Slider = () => {
     {
       image: "/images/slide1.jpg",
       text: "Klima Montaj ve Servis",
+      subText:
+        " Ünsal Teknik ile yazın keyfini çıkarın! Klima Montaj ve Servis hizmetimizle yazın sıcakları artık sorun olmaktan çıkıyor! Uzman ekibimiz, en kaliteli klima sistemlerini profesyonel bir şekilde montajını yapar ve ihtiyaçlarınıza uygun çözümler sunar.",
     },
     {
       image: "/images/slide2.jpg",
       text: "Klima Bakımı ve Onarım",
+      subText:
+        "Klima bakım ve onarım hizmetlerimizle serin kalın, rahat nefes alın! Ünsal Teknik olarak, her marka ve model klimada uzman ekiplerimizle hızlı ve güvenilir çözümler sunuyoruz.",
     },
     {
-      image: "/images/slide3.jpg",
+      image: "/images/slider3.jpg",
       text: "Havalandırma Sistemleri",
+      subText:
+        "Ünsal Teknik olarak, yaşam alanlarınızda sağlıklı ve konforlu bir atmosfer yapmayı hedefliyoruz. Modern havalandırma sistemlerimiz, iç mekanlarda hava sirkülasyonunu optimize ederken, enerji verimliliği ile bütçenize dost çözümler sunar.",
     },
   ]; // Slider'da gösterilecek görsellerin yolları
 
@@ -39,7 +45,7 @@ const Slider = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
   };
   return (
-    <div className="relative w-full mx-auto mt-10">
+    <div className="relative w-full mx-auto md:mt-0 mt-5">
       {/* Slider görüntüsü */}
       <div className="w-full overflow-hidden">
         <img
@@ -48,10 +54,13 @@ const Slider = () => {
           className=" w-full h-96 object-cover  transition duration-500"
         />
         {/* Metin Ekleme */}
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <h2 className="text-white text-2xl md:text-4xl font-bold">
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+          <h2 className="text-white text-2xl md:text-4xl font-fira font-bold">
             {slides[currentIndex].text}
           </h2>
+          <p className="text-white text-xl mx-32 mt-8">
+            {slides[currentIndex].subText}
+          </p>
         </div>
       </div>
 
