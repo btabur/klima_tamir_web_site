3fdaf14742035d12c413593801db6a5350a39bc7
Ugurtasci <ugurtasci1291@gmail.com>
2024-10-02 02:42:56 +0300

    components'a düzenlemeler ve eklemeler yapıldı

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -1,23 +1,26 @@
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
-import { menuItems } from '../utilis/menuItems'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { menuItems } from "../utilis/menuItems";
 const Menu = () => {
-    const pathname = usePathname();
-   
-    return (
-        <nav className='bg-[#326eb1] px-20 mt-4 hidden md:block'>
-            <ul className='flex justify-center items-center text-white'>
-                {menuItems.map(({ href, label }) => (
-                    <li key={href} className={`font-semibold xl:px-5 px-2 py-3 
-                    hover:bg-[#2289bb] transition-all duration-300
-                    ${pathname === href ? 'bg-[#2289bb]' : ''}`}>
-                        <Link href={href}>{label}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
-}
+  const pathname = usePathname();
 
-export default Menu
+  return (
+    <nav className="bg-[#326eb1] px-20 sticky top-14 left-0 right-0 z-40 hidden md:block">
+      <ul className="flex justify-center items-center text-white">
+        {menuItems.map(({ href, label }) => (
+          <li
+            key={href}
+            className={`font-semibold xl:px-5 px-2 py-3 
+                    hover:bg-[#2289bb] transition-all duration-300
+                    ${pathname === href ? "bg-[#2289bb]" : ""}`}
+          >
+            <Link href={href}>{label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,28 +1,27 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_WEB;
 
 console.log(MONGODB_URL);
-
 
 const connect = async () => {
   const connectionState = mongoose.connection.readyState;
 
   if (connectionState === 1) {
-    console.log('Already connected');
+    console.log("Already connected");
     return;
   }
 
   if (connectionState === 2) {
-    console.log('Connecting...');
+    console.log("Connecting...");
     return;
   }
 
   try {
     await mongoose.connect(MONGODB_URL, {
-      dbName: "klima_tamir",  // Veritabanı adınız doğru mu?
-      serverSelectionTimeoutMS: 5000,  // 5 saniye zaman aşımı
-      connectTimeoutMS: 10000,  // 10 saniye bağlantı zaman aşımı
+      dbName: "klima_tamir", // Veritabanı adınız doğru mu?
+      serverSelectionTimeoutMS: 5000, // 5 saniye zaman aşımı
+      connectTimeoutMS: 10000, // 10 saniye bağlantı zaman aşımı
     });
 
     console.log("Connected to MongoDB");
